55bee0eb207878fba24a7ea8f3581f58059fb956
Sebastian Klemke <packet@nerdheim.de>
2021-02-07 01:26:18 +0100

    chore: Bump version

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ with open('README.md', 'r') as fh:
 
 setup(
     name='pyresidfp',
-    version='0.2',
+    version='0.3.dev0',
     author='Sebastian Klemke',
     author_email='pypi@nerdheim.de',
     description='Emulates the SID sound-chip',
