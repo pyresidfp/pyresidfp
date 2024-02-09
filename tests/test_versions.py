a22b530c45620cb484cfe3c8eedeb2d2437d845e
Sebastian Klemke <3669903+packet23@users.noreply.github.com>
2024-02-09 11:41:21 +0100

    style: r"" instead of r''

--- tests/test_versions.py
+++ tests/test_versions.py
@@ -4,9 +4,9 @@ import pyresidfp
 import pyresidfp._pyresidfp as _pyresidfp
 
 
-BASE_VERSION_REGEX = re.compile(r'^([0-9]+\.[0-9]+(\.[0-9]+)?)(\.[^.]*)*$')
+BASE_VERSION_REGEX = re.compile(r"^([0-9]+\.[0-9]+(\.[0-9]+)?)(\.[^.]*)*$")
 
 
 def test_version_matches_closely():
-    base_version = BASE_VERSION_REGEX.sub(r'\1', pyresidfp.__version__) 
+    base_version = BASE_VERSION_REGEX.sub(r"\1", pyresidfp.__version__)
     assert base_version == _pyresidfp.__version__
