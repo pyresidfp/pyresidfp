7dc6dcd27836fe4301537955079d430b6dcf9def
Sebastian Klemke <3669903+packet23@users.noreply.github.com>
2024-02-09 11:28:37 +0100

    fix: apply same version logic for tests

--- tests/test_versions.py
+++ tests/test_versions.py
@@ -1,6 +1,12 @@
+import re
+
 import pyresidfp
 import pyresidfp._pyresidfp as _pyresidfp
 
 
-def test_version_matches():
-    assert pyresidfp.__version__ == _pyresidfp.__version__
+BASE_VERSION_REGEX = re.compile(r'^([0-9]+\.[0-9]+(\.[0-9]+)?)(\.[^.]*)*$')
+
+
+def test_version_matches_closely():
+    base_version = BASE_VERSION_REGEX.sub(r'\1', pyresidfp.__version__) 
+    assert base_version == _pyresidfp.__version__
